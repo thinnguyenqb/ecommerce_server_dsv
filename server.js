9f7ec064817c79c7fa19a31591eaf4b74894ca6e
EricNguyen99 <vanthin7111999@gmail.com>
2022-03-15 15:49:06 +0700

    init project

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Connect to mongodb
+const URL = process.env.MONGODB_URL;
+mongoose.connect(URL, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, err => {
+    if (err) throw err;
+    console.log("Connected to mongdb!");
+})
+
+app.get('/', (req, res, next) => {
+    res.json({msg: "Hello everyone!"});
+});
+
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+});
